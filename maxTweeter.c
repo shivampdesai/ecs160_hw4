7eeeba39c68562d9f88840d8a516342846467500
Rohit Dhamankar <rdhamankar@ucdavis.edu>
2019-11-29 13:14:25 -0800

    refactored and cleaned some code

--- maxTweeter.c
+++ maxTweeter.c
@@ -27,123 +27,123 @@ void error()
 }
 
 bool validColumns(char* line){
-  int comma_count = 0;
-  for (int i = 0; i < strlen(line); i++){
-    if (line[i] == ','){
-      comma_count++;
+    int comma_count = 0;
+    for (int i = 0; i < strlen(line); i++){
+        if (line[i] == ','){
+            comma_count++;
+        }
     }
-  }
 
-  return comma_count == expected_columns;
+    return comma_count == expected_columns;
 }
 
 int getNameColumn(char* line){
 
-  char* quote_ptr = strstr(line, "\"name\"");
-  char* pos_ptr = strstr(line, "name");
+    char* quote_ptr = strstr(line, "\"name\"");
+    char* pos_ptr = strstr(line, "name");
 
-  if (quote_ptr + 1 == pos_ptr){
-    pos_ptr = strstr(pos_ptr + 1, "name");
-  }
-
-  if ((pos_ptr == NULL && quote_ptr == NULL) || (quote_ptr && pos_ptr)){
-    error();
-  }
-
-  char* ptr = NULL;
-
-  if (quote_ptr){
-    if (strstr(quote_ptr + 2, "name")){
-      error();
+    if (quote_ptr + 1 == pos_ptr){
+        pos_ptr = strstr(pos_ptr + 1, "name");
     }
 
-    name_quoted = true;
-    ptr = quote_ptr;
-  } else if (pos_ptr) {
-    if (strstr(pos_ptr + 1, "name")){
-      error();
-    }
-
-    ptr = pos_ptr;
-  } else {
-    error();
-  }
-
-  int comma_count = 0;
-  for (int i = 0; i < strlen(ptr); i++){
-    if (ptr[i] == ','){
-      comma_count++;
-    }
-  }
-
-  return expected_columns - comma_count;
-}
-
-char* trim(char* name){
-  int quote_count = 0;
-
-  if (strlen(name) > 1){
-    quote_count = name[0] == '\"'? 1 : 0;
-    quote_count = name[strlen(name) - 1] == '\"'? quote_count + 1 : quote_count;
-
-    switch (quote_count) {
-      case 0:
-        if (name_quoted){
-          error();
-        }
-
-        return name;
-      case 1:
+    if ((pos_ptr == NULL && quote_ptr == NULL) || (quote_ptr && pos_ptr)){
         error();
-        break;
-      case 2:
-        if (!name_quoted){
+    }
+
+    char* ptr = NULL;
+
+    if (quote_ptr){
+        if (strstr(quote_ptr + 2, "name")){
             error();
         }
 
-        name++;
-        char* temp = malloc((strlen(name) - 1) * 4);
-
-        int index = 0;
-        while (name[index + 1] != '\0'){
-            temp[index] = name[index];
-            index++;
+        name_quoted = true;
+        ptr = quote_ptr;
+    } else if (pos_ptr) {
+        if (strstr(pos_ptr + 1, "name")){
+            error();
         }
 
-        return temp;
-        break;
-      default:
-        return name;
+        ptr = pos_ptr;
+    } else {
+        error();
     }
 
-  }
+    int comma_count = 0;
+    for (int i = 0; i < strlen(ptr); i++){
+        if (ptr[i] == ','){
+            comma_count++;
+        }
+    }
 
-  return name;
+    return expected_columns - comma_count;
+}
+
+char* trim(char* name){
+    int quote_count = 0;
+
+    if (strlen(name) > 1){
+        quote_count = name[0] == '\"'? 1 : 0;
+        quote_count = name[strlen(name) - 1] == '\"'? quote_count + 1 : quote_count;
+
+        switch (quote_count) {
+            case 0:
+            if (name_quoted){
+                error();
+            }
+
+            return name;
+            case 1:
+            error();
+            break;
+            case 2:
+            if (!name_quoted){
+                error();
+            }
+
+            name++;
+            char* temp = malloc((strlen(name) - 1) * 4);
+
+            int index = 0;
+            while (name[index + 1] != '\0'){
+                temp[index] = name[index];
+                index++;
+            }
+
+            return temp;
+            break;
+            default:
+            return name;
+        }
+
+    }
+
+    return name;
 
 }
 
 char* getfield(char* line, int num)
 {
-  int comma_count = 0;
-  char* name = malloc(1024 * 4);
+    int comma_count = 0;
+    char* name = malloc(MAX_LINE_LENGTH * 4);
 
-  for (int i = 0; i < strlen(line); i++){
-    if (comma_count == num){
-      int index = 0;
-      while (line[i + index] != ','){
-        name[index] = line[i + index];
-        index++;
-      }
+    for (int i = 0; i < strlen(line); i++){
+        if (comma_count == num){
+            int index = 0;
+            while (line[i + index] != ','){
+                name[index] = line[i + index];
+                index++;
+            }
 
-      return trim(name);
+            return trim(name);
+        }
+
+        if (line[i] == ','){
+            comma_count++;
+        }
     }
 
-    if (line[i] == ','){
-      comma_count++;
-    }
-  }
-
-  return "NULL";
+    return "NULL";
 }
 
 
@@ -160,28 +160,69 @@ long hashCode(const char *str) {
 
 void iterate(){
     for (int i = 0; i < MAX_SIZE; i++){
-      if (hashmap[i].name != NULL){
-        printf("%s: %d\n", hashmap[i].name, hashmap[i].count);
-      }
+        if (hashmap[i].name != NULL){
+            printf("%s: %d\n", hashmap[i].name, hashmap[i].count);
+        }
     }
 }
 
 int comparator(const void* p1, const void* p2)
 {
-   int first = ((struct tweet *)p1)->count;
-   int second = ((struct tweet *)p2)->count;
-   return second - first;
+    int first = ((struct tweet *)p1)->count;
+    int second = ((struct tweet *)p2)->count;
+    return second - first;
 }
 
-int getNumColumns(char* line){
-  int comma_count = 0;
-  for (int i = 0; i < strlen(line); i++){
-    if (line[i] == ','){
-      comma_count++;
+int getNumColumns(char* line)
+{
+    int comma_count = 0;
+    for (int i = 0; i < strlen(line); i++){
+        if (line[i] == ','){
+            comma_count++;
+        }
     }
-  }
 
-  return comma_count;
+    return comma_count;
+}
+
+void mapInsert(char* name)
+{
+    long hashcode = hashCode(name);
+
+    if (hashmap[hashcode % MAX_SIZE].name == NULL)
+    {
+        hashmap[hashcode % MAX_SIZE].name = malloc(strlen(name) * 4);
+        strcpy(hashmap[hashcode % MAX_SIZE].name, name);
+        hashmap[hashcode % MAX_SIZE].count = 1;
+    }
+    else
+    {
+        if (strcmp(hashmap[hashcode % MAX_SIZE].name, name) == 0)
+        {
+            hashmap[hashcode % MAX_SIZE].count += 1;
+        }
+        else
+        {
+            long newIndex = 0;
+            while (hashmap[newIndex].name != NULL)
+            {
+                newIndex++;
+            }
+
+            hashmap[newIndex].name = malloc(strlen(name) * 4);
+            strcpy(hashmap[newIndex].name, name);
+            hashmap[newIndex].count = 1;
+        }
+    }
+}
+
+void outputTopN(int n)
+{
+    qsort((void*)hashmap, sizeof(hashmap) / sizeof(hashmap[0]), sizeof(hashmap[0]), comparator);
+    for(int i = 0; i < n; i++)
+    {
+        printf("%s: %d\n", hashmap[i].name, hashmap[i].count);
+    }
 }
 
 int main(int argc, char* argv[])
@@ -198,63 +239,39 @@ int main(int argc, char* argv[])
         error();
     }
 
-    char line[1024];
-    char temp[1024];
-    fgets(line, 1024, stream);
+    char line[2 * MAX_LINE_LENGTH];
+    char temp[2 * MAX_LINE_LENGTH];
+    fgets(line, 2 * MAX_LINE_LENGTH, stream);
+    if (strlen(line) > MAX_LINE_LENGTH) {
+        error();
+    }
     strcpy(temp, line);
 
     expected_columns = getNumColumns(temp);
 
     if (!validColumns(temp)){
-      error();
+        error();
     }
 
     int nameColumn = getNameColumn(temp);
 
-    int i = 0;
-    while (fgets(line, 2048, stream))
+    while (fgets(line, 2 * MAX_LINE_LENGTH, stream))
     {
-
-      if (strlen(line) > MAX_LINE_LENGTH) {
-        error();
-      }
+        if (strlen(line) > MAX_LINE_LENGTH) {
+            error();
+        }
 
         if (line_count == MAX_SIZE){
-          error();
+            error();
         }
 
         strcpy(temp, line);
 
         char* name = getfield(temp, nameColumn);
-        long hashcode = hashCode(name);
-
-        if (hashmap[hashcode % MAX_SIZE].name == NULL){
-            hashmap[hashcode % MAX_SIZE].name = malloc(strlen(name) * 4);
-            strcpy(hashmap[hashcode % MAX_SIZE].name, name);
-            hashmap[hashcode % MAX_SIZE].count = 1;
-        } else {
-            if (strcmp(hashmap[hashcode % MAX_SIZE].name, name) == 0){
-              hashmap[hashcode % MAX_SIZE].count += 1;
-            } else {
-              long newIndex = 0;
-              while (hashmap[newIndex].name != NULL){
-                newIndex++;
-              }
-
-              hashmap[newIndex].name = malloc(strlen(name) * 4);
-              strcpy(hashmap[newIndex].name, name);
-              hashmap[newIndex].count = 1;
-            }
-        }
+        mapInsert(name);
 
         line_count++;
 
     }
-
-	qsort((void*)hashmap, sizeof(hashmap) / sizeof(hashmap[0]), sizeof(hashmap[0]), comparator);
-    for(int i = 0; i < 10; i++)
-    {
-        printf("%s: %d\n", hashmap[i].name, hashmap[i].count);
-    }
-
+    outputTopN(10);
 }
